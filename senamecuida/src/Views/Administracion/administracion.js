a14b463205d193df2ea4db730576692a430f8c86
LeidyAl20 <58597132+LeidyAl20@users.noreply.github.com>
2020-06-19 16:30:00 -0500

    Estilos básicos del componente Table

--- senamecuida/src/Views/Administracion/administracion.js
+++ senamecuida/src/Views/Administracion/administracion.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 
 import './estilos.css';
 import NavAdmin from '../../Components/Navs/NavAdmin';
+import Table from '../../Components/Table/Table';
 
 
 class Admin extends Component{
     render(){
         return(
-            <NavAdmin></NavAdmin>
+            <div className='containerAdmin'>
+                <NavAdmin></NavAdmin>
+                <div className='contAdmin'>
+                    <Table/>
+                </div>
+            </div>
         )
     }
 }
