5b4481e3f2955cca068e8bc3ba4decebe43e41fc
LeidyAl20 <58597132+LeidyAl20@users.noreply.github.com>
2020-06-23 15:31:35 -0500

    Se agrega menu desplegable en la administración

--- senamecuida/src/Components/Navs/NavAdmin.js
+++ senamecuida/src/Components/Navs/NavAdmin.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle, faHome, faUsers,faBars} from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle, faHome, faUsers, faBars } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown } from 'react-bootstrap';
 import { Search } from '../common/Inputs';
 import LogoSena from '../../Assets/image/logo/LogoSena.png';
 import './estilos.css';
+import { Link } from 'react-router-dom';
 
 const NavAdmin = ({ }) => {
     return (
@@ -11,7 +13,16 @@ const NavAdmin = ({ }) => {
             <div className='adminTop'>
                 <div className='txt'>
                     <h2>SENA ME CUIDA</h2>
-                    <FontAwesomeIcon icon={faBars} className='iconNavTop'/>
+                    <Dropdown className='contIconNavTop'>
+                        <Dropdown.Toggle variant="outline-light" className='iconNavTop'>
+                            <FontAwesomeIcon icon={faBars} />
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item><Link to='/Admin1'>Inicio</Link></Dropdown.Item>
+                            <Dropdown.Item><Link to='/'>Usuarios</Link></Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
                 <div className='contRight'>
                     <div className='search'>
@@ -24,7 +35,7 @@ const NavAdmin = ({ }) => {
             </div>
             <div className='adminLateral'>
                 <div className='logo'>
-                    <img src={LogoSena} alt=""/>
+                    <img src={LogoSena} alt="" />
                 </div>
                 <div className='menu'>
                     <ul>
